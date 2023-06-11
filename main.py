e06b6abcfde03b3cbadfc54714072d5605da76e2
Alekei Grachev <ax66@bk.ru>
2023-06-11 22:57:34 +0300

    implemented main.pu

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
-from PIL import Image, ImageDraw
+import datetime
+from pathlib import Path
+from PIL import Image, ImageDraw, ImageFont
 
-# Create a blank canvas with a white background
-canvas = Image.new('RGB', (70, 7), (255, 255, 255))
+def get_pixels(text: str) -> list[tuple[int, int, int]]:
+    width, height = 700, 7
+    canvas = Image.new('RGB', (width, height), 'black')
 
-# Create a draw object
-draw = ImageDraw.Draw(canvas)
+    draw = ImageDraw.Draw(canvas)
+    draw.text((0, -3), text, fill="white")
+    canvas.save('print_test.png')
 
-# Define the text and font
-text = "hello"
-font_size = 20  # Adjust font size as needed
+    pixels = [canvas.getpixel((x, y))
+              for x in range(width)
+              for y in range(height)]
+    return pixels
 
-# Calculate the position to center the text
-text_width, text_height = draw.textsize(text)
-x = (canvas.width - text_width) // 2
-y = (canvas.height - text_height) // 2
+def get_commit_count(start_date, pixels, max_commits):
+    offset_days = (datetime.datetime.utcnow() - start_date).days
+    pixel = pixels[offset_days]
+    MAX_PIXEL = (255,255,255)
+    commits = 1.0 * max_commits * sum(pixel) / sum(MAX_PIXEL)
+    return int(commits)
 
-# Draw the text on the canvas
-draw.text((x, y), text, fill=(0, 0, 0))  # Using black color for text
 
-# Save the canvas as an image file
-canvas.save('canvas.png')
-
-# Get the colors of each pixel in the canvas
-pixels = list(canvas.getdata())
-
-# Display the colors
-for pixel in pixels:
-    print(pixel)
-
+if __name__ == '__main__':
+    START_DATE = '2023-06-11'
+    pixels = get_pixels(text='python')
+    start_date = datetime.datetime.strptime(START_DATE, '%Y-%m-%d')
+    count = get_commit_count(start_date=start_date, pixels=pixels, max_commits=45)
+    print(count)
