f64641f725188c24196ffff842e6b24615fbbc1c
Alekei Grachev <ax66@bk.ru>
2023-06-11 21:02:23 +0300

    no error if no branch

--- make_commits.sh
+++ make_commits.sh
@@ -10,15 +10,16 @@ COMMIT_COUNT=${1:-2}
 #WORK_PATH=/tmp/repo_$(uuidgen)
 
 echo creating branch: ${BRANCH_NAME}
-git branch ${BRANCH_NAME}
-git checkout ${BRANCH_NAME}
-# git push --set-upstream origin ${BRANCH_NAME}
 
-for i in $( seq 0 ${COMMIT_COUNT} )
+git rev-parse --verify --quiet ${BRANCH_NAME} || \
+  git branch ${BRANCH_NAME} && \
+  git push --set-upstream origin ${BRANCH_NAME}
+
+for i in $( seq 1 ${COMMIT_COUNT} )
 do
 	echo "change" >> changes
 	git add changes
 	git commit -m "change"
-	git push --set-upstream origin ${BRANCH_NAME}
+	git push 
 done
 
